1bf78aab3375e6f55c060e7481bf8a019db0ad36
kiri11-00 <131610191+kiri11-00@users.noreply.github.com>
2023-04-25 18:31:39 +0300

    111111

--- send_email.php
+++ send_email.php
@@ -1,12 +1,13 @@
 <?php
 
-if(isset($_POST['submit'])) {
-    $to = $_POST['email'];; // электронный адрес получателя
+if(isset($_POST['forgot'])) {
+    $to = "1903kirill04@mail.ru"; // электронный адрес получателя
     $subject = $_POST['subject']; // тема сообщения, полученная из формы
     $message = $_POST['message']; // сообщение, полученное из формы
-    $from = "1903kirill04@mail.ru"; // электронный адрес отправителя, полученный из формы
-    $headers = "From:" . $from;
-    
+    $from = $_POST['email']; // электронный адрес отправителя, полученный из формы
+    $headers = 'From: kiri11-00@mail.ru' . "\r\n" .
+    'Reply-To: 1903kirill04@mail.ru' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
     if(mail($to,$subject,$message,$headers)) {
         echo "Сообщение успешно отправлено.";
     } else {
