9ed9f5072aead75008bc5aefc670dc63be0f6e21
kiri11-00 <131610191+kiri11-00@users.noreply.github.com>
2023-04-25 16:38:27 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,11 +1,17 @@
 <?php
-$to = $_POST['email'];
-$subject = 'Привет';
-$message = 'Привет!';
-$headers = 'From: kiri11-00@mail.ru' . "\r\n" .
-    'Reply-To: 1903kirill04@mail.ru' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
-echo 'Письмо отправлено!';
+if(isset($_POST['submit'])) {
+    $to = $_POST['email'];; // электронный адрес получателя
+    $subject = $_POST['subject']; // тема сообщения, полученная из формы
+    $message = $_POST['message']; // сообщение, полученное из формы
+    $from = "1903kirill04@mail.ru"; // электронный адрес отправителя, полученный из формы
+    $headers = "From:" . $from;
+    
+    if(mail($to,$subject,$message,$headers)) {
+        echo "Сообщение успешно отправлено.";
+    } else {
+        echo "Ошибка отправки сообщения.";
+    }
+}
+
 ?>
